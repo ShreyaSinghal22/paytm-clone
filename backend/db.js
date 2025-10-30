f1b2efb2a8286a7f422e2e95da36b12eef7e91c4
ShreyaSinghal22 <shreyasinghal5257@gmail.com>
2025-10-30 15:06:00 +0530

    made some slight changes

---
+++ backend/db.js
@@ -0,0 +1,20 @@
+const mongoose = require ("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/mymongo");
+
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: {type: String, required: true },
+    firstname: String,
+    Lastname: String,
+    email: { type: String, required:true, unique: true, match: [/.+\@.+\..+/, "Please fill a valid email address"]},
+    Age: {type: Number, required: true},
+});
+
+const user = mongoose.model('user', userSchema);
+
+module.exprorts = {
+    user
+};
+
